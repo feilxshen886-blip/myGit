6c32f6efe2e4996d05377090f017468e0d20bec6
junqi <feilxshen886@gmail.com>
2026-04-07 10:41:59 +0800

    deploy

---
+++ assets/reward-D955t3nh.js
@@ -0,0 +1,1 @@
+import{B as t}from"./index-BbmDH_7C.js";const s=t("reward",{state:()=>({tableData:[]}),actions:{setTableData(a){this.tableData=a},addRow(a){this.tableData.push(a)},deleteRow(a){this.tableData.splice(a,1)}}});export{s as u};
